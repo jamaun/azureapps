c294694fd6436bbc4596329bf337c6eb0ca16536
Aun Sadiq <Aun.Sadiq@bentley.com>
2018-10-15 16:10:07 +0500

    Add project files.

---
+++ ams-AspCoreNetApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ams_AspCoreNetApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ams-WebApplication2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ams_WebApplication2.MvcApplication" Language="C#" %>
